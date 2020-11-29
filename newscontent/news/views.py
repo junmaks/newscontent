ad42ed69c306081f9d8adbdb0e3ef9d96abf96fa
junmaks <maxmiassspb@gmail.com>
2020-11-29 20:38:46 +0300

    Убрал вызов категорий в отдельный custom tags (news_tags)

--- newscontent/news/views.py
+++ newscontent/news/views.py
@@ -8,19 +8,19 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories
     }
     return render(request=request, template_name='news/index.html',
                   context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    context = {'news': news, 'categories': categories, 'category': category}
+    context = {
+        'news': news,
+        'category': category
+    }
     return render(request=request, template_name='news/category.html',
                   context=context)
