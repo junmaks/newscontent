cebd66ea6cd66e438988f421aeb9816d2c5020a9
junmaks <maxmiassspb@gmail.com>
2020-11-30 17:00:45 +0300

    Описано добавление новости

--- newscontent/news/views.py
+++ newscontent/news/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
+from .forms import NewsForm
 
 # Create your views here.
 
@@ -30,3 +30,19 @@ def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id) # возвращает 404, если объект модели не найден
     return render(request=request, template_name='news/view_news.html', context={'news_item': news_item})
+
+
+def add_news(request,):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data) # распаковка словаря
+            # title = form.cleaned_data(title= 'title')
+            return redirect(news)
+    else:
+        form = NewsForm()
+    context = {
+        'form': form
+    }
+    return render(request=request, template_name='news/add_news.html', context=context)
