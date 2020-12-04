0fe8ce7aeebdb73c46c1fb3dcff91a2843f8767b
junmaks <maxmiassspb@gmail.com>
2020-12-04 15:03:48 +0300

    Создана модель Account с токенаутентификацией

---
+++ newscontent/users/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from newscontent import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import AbstractUser
+
+
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+#
+# class CustomUser(AbstractUser):
+#     date_both = models.DateField(verbose_name='Дата рождения', blank=True, null=True)
+#
+#     class Meta:
+#         verbose_name = 'Пользователи'
+#         verbose_name_plural = 'Пользователи'
+#         ordering = ['username']
+#
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
+
+
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    username = models.CharField(max_length=30, unique=True, verbose_name='Логин')
+    email = models.EmailField(verbose_name='Email', max_length=255, unique=True, )
+    # date_of_birth = models.DateField(verbose_name='Дата рождения')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = MyAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
