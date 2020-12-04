08000e4f40790d34a87c031f5c183e94e1fa32e7
junmaks <maxmiassspb@gmail.com>
2020-12-04 15:04:37 +0300

    Форма создания юзера, авторизации

---
+++ newscontent/users/forms.py
@@ -0,0 +1,148 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+# from .models import CustomUser
+#
+#
+# class CustomUserCreationForm(UserCreationForm):
+#     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+#     password2 = forms.CharField(label='Подтвеждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+#     class Meta:
+#         model = CustomUser
+#         fields = ('username', 'email', 'date_both', 'password1', 'password2')
+#         widgets = {
+#             'username': forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+#             'email': forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+#             'date_both': forms.DateInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+#             'password1': forms.PasswordInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+#             'password2': forms.PasswordInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+#
+#         }
+#
+#
+#     def clean_username(self):
+#         username = self.cleaned_data.get('username')
+#         qs = CustomUser.objects.filter(username=username)
+#         if qs.exists():
+#             raise forms.ValidationError("Пользователь с таким логином уже зарегистрирован")
+#         return username
+#
+#     def clean_email(self):
+#         email = self.cleaned_data.get('email')
+#         qs = CustomUser.objects.filter(email=email)
+#         if qs.exists():
+#             raise forms.ValidationError("Пользователь с такой почтой уже зарегистрирован")
+#         return email
+#
+#
+#
+# class CustomUserChangeForm(UserChangeForm):
+#
+#     class Meta:
+#         model = CustomUser
+#         fields = ('username', 'email')
+#
+#
+# class UserLoginForm(AuthenticationForm):
+#     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', }))
+#     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', }))
+#
+
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import Account
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль',widget=forms.PasswordInput(attrs={'class': 'form-control', }))
+    password2 = forms.CharField(label='Повторно введите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', }))
+
+    class Meta:
+        model = Account
+        fields = ('username', 'email',)
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+        }
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = Account.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("Пользователь с таким логином уже зарегистрирован")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = Account.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("Данный email зарегистрирован")
+        return email
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self):
+        user = super(RegisterForm, self).save()
+        user.set_password(self.cleaned_data["password2"])
+        user.save()
+        return user
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ('username', 'email', )
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Account
+        fields = ('email', 'password', )
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', }))
+    password = forms.CharField(label='Пароль',widget=forms.PasswordInput(attrs={'class': 'form-control', }))
+
